87578736477b19104a38d9dfd23b9d587d0578d2
godanielgo <me@godanielgo.com>
2024-04-17 21:23:04 +0800

    Class assignment changes

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 const print = require("./controller");
 
+// Existing endpoint
 app.get('/', print);
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`Listening to port ${process.env.PORT}`)
-})
+// New endpoint for /home
+app.get('/home', (req, res) => {
+    res.send("This is the home page");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening to port ${process.env.PORT}`);
+});
