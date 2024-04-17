40e83bfb1974216f63d22c9cd3f4f2869480cfe3
godanielgo <me@godanielgo.com>
2024-04-17 21:06:50 +0800

    Class assignment changes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
-const express = require('express')
-const app = express()
+function printHome(req, res) {
+    res.send("This is the home page");
+}
 
-const print = require("./controller");
+function createUser(req, res) {
+    const { name, email, password } = req.body;
+    // Here you might want to add logic to handle the user creation, e.g., validation, saving to a database, etc.
+    res.send(`Name: ${name}, Email: ${email}`);
+}
 
-app.get('/', print);
-
-app.listen(process.env.PORT, ()=>{
-    console.log(`Listening to port ${process.env.PORT}`)
-})
+module.exports = { printHome, createUser };
